4211f5ea35d2f6a2395eddae7810bc050ba39f2d
Merveille94 <emanueleguehi@gmail.com>
2024-09-06 06:44:02 +0000

    Spinners

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,13 +1,12 @@
 import ScrollToTop from "../component/ScrollToTop.jsx";
 import Navbar from "../component/Navbar.jsx";
 import {Outlet} from "react-router-dom";
-import Footer from "../component/Footer.jsx";
 
 // Here is the layout that we will use in all our pages
 const MainLayout = () => {
     return (
         <>
-            {/*as you can see scroll to the top is above to do what is suppose to when the time comes*/}
+            {/*as you can see scroll to the top is above to do what is supposed to when the time comes*/}
             <ScrollToTop />
             <Navbar />
             <Outlet/>
