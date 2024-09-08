79a9588eaaf03c3bcea58920dddc754e9a11a9c0
Merveille94 <emanueleguehi@gmail.com>
2024-09-08 07:42:08 +0000

    Spinners

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,6 +1,6 @@
 import Hero from "../component/Hero.jsx";
 
-// Here is the landing page structure and content notice it only contains Hero that is because of the layout we chose
+// Here is the landing page structure and content notice it only contains Hero that is because of the layout
 const LandingPage = () => {
     return (
         <>
