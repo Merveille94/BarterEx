205d4a450c1e31c38936b69a0850bce4b83f1067
Merveille94 <emanueleguehi@gmail.com>
2024-09-12 15:28:48 +0000

    Contact page and Banner

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -1,13 +1,17 @@
+import {useState} from "react";
 import data from '../data.jsx';  // No need to import 'aboutSection' separately
 import Footer from "./Footer.jsx";
 import Banner from "./Banner.jsx";
+import {FaArrowDownWideShort, FaArrowUpWideShort} from "react-icons/fa6";
 
 const About = () => {
     const content = data.heroSection[0]; // Access the first item in heroSection
     const aboutContent = data.aboutSection; // Access aboutSection from data
+    const [showFullText, setShowFullText] = useState(false);
+    const toggleText = () => setShowFullText(!showFullText);
 
     return (
-        <section className='w-full h-screen relative flex flex-col items-center justify-between pt-10'>
+        <section className='w-full h-screen relative flex flex-col items-center justify-between pt-12'>
             {/* Background Image */}
             <img className="h-full w-full absolute inset-0 bg-center bg-cover object-cover z-0"
                 alt="background image"
@@ -16,17 +20,21 @@ const About = () => {
 
             <div className='container mx-auto relative z-5 p-6 text-slate-300 flex flex-col justify-center items-center my-auto'>
                 {/* About Section */}
-                <div className='flex flex-col items-center justify-center bg-backgroundGray md:py-4 md:px-auto py-2 px-4 rounded-xl'>
+                <div
+                    className='flex flex-col items-center justify-center bg-backgroundGray md:py-4 md:px-auto py-2 px-4 rounded-xl'>
                     <h2 className='flex items-center justify-center bg-customGray text-black md:px-auto py-2 px-10 rounded-xl'>About</h2>
 
                     {/* About Section Text */}
-                    <div className='p-4 mx-auto'>
+                    <div className={`p-4 mx-auto ${showFullText ? '' : 'max-h-96 md:max-h-svh overflow-hidden'}`} style={{lineHeight: '1.75'}}>
                         {aboutContent.map((paragraph, index) => (
-                            <p key={index} className="my-4 text-justify text-slate-200">
+                            <p key={index} className="my-4 text-justify text-slate-200" >
                                 {paragraph}
                             </p>
                         ))}
                     </div>
+                    <div className='text-slate-100 md:hidden p-2' onClick={toggleText}>{showFullText ?
+                        <FaArrowUpWideShort size={25} className='text-slate-300'/> :
+                        <FaArrowDownWideShort size={25} className='text-slate-300'/>}</div>
                 </div>
             </div>
             <Banner/>
