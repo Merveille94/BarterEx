c9f1a35973ed42d640688d5238ea2d6f34906feb
Merveille94 <emanueleguehi@gmail.com>
2024-09-12 11:31:40 +0000

    Spinners

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -57,28 +57,24 @@ const Hero = () => {
                     </div>
                 </div>
             </div>
-            <div className="bg-customGray h-auto w-full text-slate-100 z-0 flex items-center justify-center p-4">
-                <div className="container mx-auto grid grid-cols-1 md:grid-cols-3 grid-rows-1 p-8 gap-5">
-                    <div
-                        className="col-span-1 row-span-1 text-center text-slate-100 bg-gray-600 md:py-4 md:px-auto py-2 px-8 rounded-tr-xl rounded-tl-xl hover:-translate-y-4 duration-500 ease-out">
+            <div className="bg-customGray h-auto w-full text-slate-100 z-0 flex items-center justify-center p-3">
+                <div className="container mx-auto grid grid-cols-1 md:grid-cols-4 grid-rows-1 p-4 gap-3">
+                    <div className="col-span-1 row-span-1 text-center text-slate-100 bg-gray-600 md:py-4 md:px-auto py-2 px-4 rounded-tr-xl rounded-tl-xl hover:-translate-y-4 duration-500 ease-out">
                         <h2>
                             <Link to={"/exchange"}>Get Started</Link>
                         </h2>
                     </div>
-                    <div
-                        className="col-span-1 row-span-1 text-center text-slate-100 bg-gray-600 md:py-4 md:px-auto py-2 px-8 rounded-tr-xl rounded-tl-xl hover:-translate-y-4 duration-700 ease-out">
+                    <div className="col-span-1 row-span-1 text-center text-slate-100 bg-gray-600 md:py-4 md:px-auto py-2 px-4 rounded-tr-xl rounded-tl-xl hover:-translate-y-4 duration-700 ease-out">
                         <h2>
                             <Link to={"/overview"}>Instructions</Link>
                         </h2>
                     </div>
-                    <div
-                        className="col-span-1 row-span-1 text-center text-slate-100 bg-gray-600 md:py-4 md:px-auto py-2 px-8 rounded-tr-xl rounded-tl-xl hover:-translate-y-4 duration-700 ease-out">
+                    <div className="col-span-1 row-span-1 text-center text-slate-100 bg-gray-600 md:py-4 md:px-auto py-2 px-4 rounded-tr-xl rounded-tl-xl hover:-translate-y-4 duration-700 ease-out">
                         <h2>
                             <Link to={"/overview"}>About</Link>
                         </h2>
                     </div>
-                    <div
-                        className="col-span-1 row-span-1 text-center text-slate-100 bg-gray-600 md:py-4 md:px-auto py-2 px-8 rounded-tr-xl rounded-tl-xl hover:-translate-y-4 duration-500 ease-out">
+                    <div className="col-span-1 row-span-1 text-center text-slate-100 bg-gray-600 md:py-4 md:px-auto py-2 px-4 rounded-tr-xl rounded-tl-xl hover:-translate-y-4 duration-500 ease-out">
                         <h2>
                             <Link to={"/contact"}>Contacts</Link>
                         </h2>
