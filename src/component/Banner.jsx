f5c9cbb08e4de0e2d2c5bd04caae8072f51a08e8
Merveille94 <emanueleguehi@gmail.com>
2024-09-13 09:36:38 +0000

    Contact page and Banner

--- src/component/Banner.jsx
+++ src/component/Banner.jsx
@@ -5,30 +5,18 @@ const Banner = () => {
     return (
         <div className="bg-customGray h-auto w-full text-slate-100 z-0 flex items-center justify-center p-3">
             <div className="container mx-auto grid grid-cols-1 md:grid-cols-4 grid-rows-1 p-4 gap-3">
-                <div
-                    className="col-span-1 row-span-1 text-center text-slate-100 bg-gray-600 hover:bg-gray-800 md:py-4 md:px-auto py-2 px-auto rounded-tr-xl rounded-tl-xl hover:-translate-y-4 duration-500 ease-out">
-                    <h2>
-                        <Link to={"/exchange"}>Get Started</Link>
-                    </h2>
-                </div>
-                <div
-                    className="col-span-1 row-span-1 text-center text-slate-100 bg-gray-600 hover:bg-gray-800 md:py-4 md:px-auto py-2 px-auto rounded-tr-xl rounded-tl-xl hover:-translate-y-4 duration-700 ease-out">
-                    <h2>
-                        <Link to={"/instruction"}>Instructions</Link>
-                    </h2>
-                </div>
-                <div
-                    className="col-span-1 row-span-1 text-center text-slate-100 bg-gray-600 hover:bg-gray-800 md:py-4 md:px-auto py-2 px-auto rounded-tr-xl rounded-tl-xl hover:-translate-y-4 duration-700 ease-out">
-                    <h2>
-                        <Link to={"/about"}>About</Link>
-                    </h2>
-                </div>
-                <div
-                    className="col-span-1 row-span-1 text-center text-slate-100 bg-gray-600 hover:bg-gray-800 md:py-4 md:px-auto py-2 px-auto rounded-tr-xl rounded-tl-xl hover:-translate-y-4 duration-500 ease-out">
-                    <h2>
-                        <Link to={"/contact"}>Contacts</Link>
-                    </h2>
-                </div>
+                <Link to={"/exchange"}><div className="col-span-1 row-span-1 text-center text-slate-100 bg-gray-600 hover:bg-gray-800 md:py-4 md:px-auto py-2 px-auto rounded-tr-xl rounded-tl-xl hover:-translate-y-2 duration-500 ease-out">
+                    <h2>Get Started</h2>
+                </div></Link>
+                <Link to={"/instruction"}> <div className="col-span-1 row-span-1 text-center text-slate-100 bg-gray-600 hover:bg-gray-800 md:py-4 md:px-auto py-2 px-auto rounded-tr-xl rounded-tl-xl hover:-translate-y-2 duration-700 ease-out">
+                    <h2>Instructions</h2>
+                </div></Link>
+                <Link to={"/about"}><div className="col-span-1 row-span-1 text-center text-slate-100 bg-gray-600 hover:bg-gray-800 md:py-4 md:px-auto py-2 px-auto rounded-tr-xl rounded-tl-xl hover:-translate-y-2 duration-700 ease-out">
+                    <h2>About</h2>
+                </div></Link>
+                <Link to={"/contact"}><div className="col-span-1 row-span-1 text-center text-slate-100 bg-gray-600 hover:bg-gray-800 md:py-4 md:px-auto py-2 px-auto rounded-tr-xl rounded-tl-xl hover:-translate-y-2 duration-500 ease-out">
+                    <h2>Contacts</h2>
+                </div></Link>
             </div>
         </div>
     )
--- src/data.jsx
+++ src/data.jsx
@@ -52,7 +52,20 @@ const data = {
       link5: "https://www.linkedin.com/in/king-boakye-kyei-091a42131/",
       link6: "heavenonearth.boakye11@gmail.com"
     }
-
+  ],
+  instructionSection: [
+    {
+      step1: "To get started, click on the “Sign Up” button located at the top right corner of the homepage. Fill in your details, including your name, email address, and a secure password. Once you’ve completed the form, click “Submit” to create your account. You will receive a confirmation email shortly after. Follow the link in the email to verify your account.",
+      image1: "./img-i1.png"
+    },
+    {
+      step2: "After logging in, you will be directed to your dashboard. Here, you can view your profile, manage your listings, and track your transactions. Use the navigation menu on the left to access different sections of the dashboard. Each section provides specific tools and options to help you manage your account effectively.",
+      image2: "./img-i2.png"
+    },
+    {
+      step3: "To list an item for barter, go to the “List Item” section from the dashboard menu. Fill in the item details, including the title, description, category, and images. Make sure to provide accurate and detailed information to attract potential traders. Once you’ve filled in all the necessary fields, click “Submit” to post your listing. Your item will now be visible to other users on the platform.",
+      image3: "./img-i3.png"
+    }
   ]
 };
 
